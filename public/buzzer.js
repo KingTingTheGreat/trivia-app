651c03a25fe72cd509a55e3089124c6ad5d3a928
Jeffrey <kingtingthegreat@gmail.com>
2025-01-04 10:10:13 -0500

    fix buzzer sound

--- public/buzzer.js
+++ public/buzzer.js
@@ -1,10 +1,10 @@
-const buzzedIn = document.getElementById("buzzed-in");
-
 const audio = new Audio("/public/buzzer.mp3"); // Path to the sound file
 document.addEventListener("htmx:wsAfterMessage", (e) => {
-  if (!e.detail.message.includes('<tbody id="buzzed-in">')) {
+  if (!e.detail.message.includes('id="buzzed-in-body"')) {
     return;
   }
+
+  const buzzedIn = document.getElementById("buzzed-in-body");
   if (buzzedIn && buzzedIn.rows?.length > 0) {
     console.log("playing buzzer audio");
     audio.play();
